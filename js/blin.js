1b1a1dbff441274aa3e217f4fa2ffb3d2fd6651f
OlgaNaranovich <naranovicholga@gmail.com>
2018-11-23 08:34:54 +0300

    fix: font-size in header-right-content

--- js/blin.js
+++ js/blin.js
@@ -2,47 +2,47 @@
 function change1() {
 	var n = document.getElementById('new');
 	n.innerHTML = 'Новые блюда';
-	n.classList.add('new-activ');
+	n.classList.add('new-active');
 }
 function changeOut1() {
 	var n = document.getElementById('new');
 	n.innerHTML = 'NEW';
-	n.classList.remove('new-activ');
+	n.classList.remove('new-active');
 }
 
 function change2() {
 	var d = document.getElementById('discounts');
 	d.innerHTML = 'Акции и скидки';
-	d.classList.add('discounts-activ');
+	d.classList.add('discounts-active');
 }
 function changeOut2() {
 	var d = document.getElementById('discounts');
 	d.innerHTML = '%';
-	d.classList.remove('discounts-activ');
+	d.classList.remove('discounts-active');
 }
 
 function change3() {
 	var d = document.getElementById('delivery');
 	d.innerHTML = 'Доставка и оплата';
-	d.classList.add('delivery-activ');
+	d.classList.add('delivery-active');
 }
 function changeOut3() {
 	var d = document.getElementById('delivery');
 	var img = new Image();
 	d.innerHTML = '';
-	d.classList.remove('delivery-activ');
+	d.classList.remove('delivery-active');
 	d.appendChild(img);
 	img.src = 'img/circle-car.png';
 }
 
-// $(".toggle-menu").click(function() {
+// $(".toggle-nav").click(function() {
 // 	$(this).toggleClass("on");
 // 	$(".header-right_nav_ul").slideToggle();
 // 	return false;
 // 	});
 
 function toggleMenu() {
-	var tm = document.getElementById("toggle-menu");
+	var tm = document.getElementById("toggle-nav");
 	var menu = document.getElementById("header_nav");
 	
 	if (!menu.classList.contains("toggle")) {
